e11db332d4841986af6a31cc7d6ccb63232bea0b
Sourav Majumder <souravmajumderk99@gmail.com>
2024-01-04 02:38:53 +0600

    learning blockchian using go lang

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+var infuraURL = "https://mainnet.infura.io/v3/2MJEo6qegjyxHarloYlEKgpUREW"
+var ganacheURL = "http://localhost:8545"
+
+func main() {
+	client, err := ethclient.DialContext(context.Background(), ganacheURL)
+	if err != nil {
+		log.Fatal("Error to create a ethere client:%v", err)
+	}
+	defer client.Close()
+	block, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal("Error to get the block:%v", err)
+	}
+	fmt.Println(block.Number())
+}
